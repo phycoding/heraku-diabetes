d7ca67aca5bfc135eb37382e1a840cbb485056c9
Puspak <69215834+phycoding@users.noreply.github.com>
2022-05-11 20:39:37 +0530

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -22,7 +22,7 @@ def inference(row, model, feat_cols):
     if element <= 0.5:
         st.title("You are a healthy person")
     else:
-        st.title("You are likely to have {0.2f} percent of diabetes".format(result*100))
+        st.title("You are likely to have {} percent of diabetes".format(round(result*100,2)))
         
     
 
